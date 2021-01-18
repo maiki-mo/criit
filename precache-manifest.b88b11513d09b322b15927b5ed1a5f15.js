e59823175adc0cca97908d816068a1a7781a6418
michael <mike.p.monahan@gmail.com>
2021-01-18 14:10:35 -0600

    Updates

---
+++ precache-manifest.b88b11513d09b322b15927b5ed1a5f15.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a42514fd08f3196697c1a49fa8da9a2",
+    "url": "/crit-hiit/index.html"
+  },
+  {
+    "revision": "8ebbabc0fd360467cfde",
+    "url": "/crit-hiit/static/css/main.fa2503f7.chunk.css"
+  },
+  {
+    "revision": "4eeb3fd50beae64b6c8a",
+    "url": "/crit-hiit/static/js/2.f4d5474d.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/crit-hiit/static/js/2.f4d5474d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df69531d87e5f1ce986",
+    "url": "/crit-hiit/static/js/3.fc9618b8.chunk.js"
+  },
+  {
+    "revision": "8ebbabc0fd360467cfde",
+    "url": "/crit-hiit/static/js/main.8c0d2156.chunk.js"
+  },
+  {
+    "revision": "f2f9eb7ab838c3e379de",
+    "url": "/crit-hiit/static/js/runtime-main.cbeca478.js"
+  },
+  {
+    "revision": "6747ce2ef0038d0067a1afbaed941595",
+    "url": "/crit-hiit/static/media/boxing-bell.6747ce2e.wav"
+  },
+  {
+    "revision": "482bf33fb2a146b507b2d1782a9b842a",
+    "url": "/crit-hiit/static/media/pause-button.482bf33f.svg"
+  },
+  {
+    "revision": "a77a60973a54627b1bb5778c3039a5b5",
+    "url": "/crit-hiit/static/media/play-button.a77a6097.svg"
+  },
+  {
+    "revision": "9bba097a302d323a54d431e92a94c455",
+    "url": "/crit-hiit/static/media/reset-button.9bba097a.svg"
+  },
+  {
+    "revision": "0163862e84f3d06a3f792a7ad6bf7360",
+    "url": "/crit-hiit/static/media/school-bell.0163862e.wav"
+  },
+  {
+    "revision": "0cf138b60e5a733ac3935843a595ce27",
+    "url": "/crit-hiit/static/media/settings-icon.0cf138b6.svg"
+  },
+  {
+    "revision": "3a0b1d40283be9918605be1f0f336010",
+    "url": "/crit-hiit/static/media/stop-button.3a0b1d40.svg"
+  },
+  {
+    "revision": "6504c962328c976680356d931565d259",
+    "url": "/crit-hiit/static/media/user-icon.6504c962.svg"
+  },
+  {
+    "revision": "a61199521fc7ec29c551fac97072137d",
+    "url": "/crit-hiit/static/media/workout-icon.a6119952.svg"
+  }
+]);
